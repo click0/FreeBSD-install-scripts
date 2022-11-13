177a30b10db1ccc9a97162b15e5aba74c26b73ff
vlad11 <admin@support.od.ua>
2022-11-13 17:29:45 +0200

    Updated version of scripts.

--- gozfs_512b.sh
+++ gozfs_512b.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Current Version: 1.45
+# Current Version: 1.49
 
 # original script by Philipp Wuensche at http://anonsvn.h3q.com/s/gpt-zfsroot.sh
 # This script is considered beer ware (http://en.wikipedia.org/wiki/Beerware)
